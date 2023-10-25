97c345a0e6b0cb45079da49174fb789463c78104
Eduard-Epurica <epurica.eduard@gmail.com>
2023-10-25 14:09:00 +0300

    Initial Commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,14 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# free configuration
+CND_ARTIFACT_DIR_free=dist/free/production
+CND_ARTIFACT_NAME_free=RNBD451LowPowerTemperatureDemo.X.production.hex
+CND_ARTIFACT_PATH_free=dist/free/production/RNBD451LowPowerTemperatureDemo.X.production.hex
+# pro configuration
+CND_ARTIFACT_DIR_pro=dist/pro/production
+CND_ARTIFACT_NAME_pro=RNBD451LowPowerTemperatureDemo.X.production.hex
+CND_ARTIFACT_PATH_pro=dist/pro/production/RNBD451LowPowerTemperatureDemo.X.production.hex
